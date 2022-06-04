ecc1aae8c2c90cbda0aafd5419598cf00774df45
F. Souzza <filipesouzza.adm@gmail.com>
2022-06-04 00:04:08 -0300

    commit de aprimoramento da tela de login

---
+++ assets/js/validacao.js
@@ -0,0 +1,43 @@
+// MENSAGEM DE ERRO NOS CAMPOS DE LOGIN
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.',
+        patternMismatch: 'Insira o @ mais o usuário cadastrado.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'insira letra maiúscula, número e símbolo.'
+    },
+}
+
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
